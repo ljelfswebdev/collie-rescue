49dc83942564325fc79c1313cfffad9320fa1d6f
Lewis <lewis@westmidlandsmedia.co.uk>
2024-02-08 18:05:40 +0000

    add loading

--- src/app/shop/page.js
+++ src/app/shop/page.js
@@ -1,71 +1,3 @@
-// // 
-
-// "use client"
-
-// import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
-// import OAuth from 'oauth-1.0a';
-// import crypto from 'crypto';
-
-// const Shop = () => {
-//   const apiBaseUrl = process.env.NEXT_PUBLIC_WORDPRESS_WOO_API;
-//   const consumerKey = process.env.NEXT_PUBLIC_WOO_KEY;
-//   const consumerSecret = process.env.NEXT_PUBLIC_WOO_SECRET;
-//   const [products, setProducts] = useState([]);
-
-
-//   useEffect(() => {
-//     const oauth = OAuth({
-//       consumer: { key: consumerKey, secret: consumerSecret },
-//       signature_method: 'HMAC-SHA1',
-//       hash_function: (base_string, key) => {
-//         return crypto.createHmac('sha1', key).update(base_string).digest('base64');
-//       },
-//     });
-
-//     const requestData = {
-//       url: `${apiBaseUrl}/products`,
-//       method: 'GET',
-//     };
-
-//     const oauthHeaders = oauth.toHeader(oauth.authorize(requestData));
-
-//     const fetchProducts = async () => {
-//       try {
-//         const response = await axios.get(requestData.url, {
-//           headers: {
-//             ...oauthHeaders,
-//             'Content-Type': 'application/json',
-//             'Accept': 'application/json',
-//           },
-//           params: {
-//             _fields: 'id,name',
-//           },
-//         });
-
-//         const fetchedProducts = response.data;
-//         setProducts(fetchedProducts);
-//       } catch (error) {
-//         console.error('Error fetching Products:', error);
-//       }
-//     };
-
-//     fetchProducts();
-//   }, [apiBaseUrl, consumerKey, consumerSecret]);
-
-//   return (
-//     <>
-//       <h1>Shop Here</h1>
-//       <ul>
-//         {products.map(product => (
-//           <li key={product.id}>{product.name}</li>
-//         ))}
-//       </ul>
-//     </>
-//   );
-// };
-
-// export default Shop;
 
 
 "use client";
@@ -75,12 +7,14 @@ import axios from "axios";
 import OAuth from "oauth-1.0a";
 import crypto from "crypto";
 import Link from "next/link";
+import Loading from '../../components/global/loading';
 
 const Shop = () => {
     const apiBaseUrl = process.env.NEXT_PUBLIC_WORDPRESS_WOO_API;
     const consumerKey = process.env.NEXT_PUBLIC_WOO_KEY;
     const consumerSecret = process.env.NEXT_PUBLIC_WOO_SECRET;
   const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     const oauth = OAuth({
       consumer: { key: consumerKey, secret: consumerSecret },
@@ -108,14 +42,21 @@ const Shop = () => {
           params: oauth.authorize(requestData),
         });
         const fetchedProducts = response.data;
-        console.log(fetchedProducts);
         setProducts(fetchedProducts);
+        setIsLoading(false);
       } catch (error) {
         console.error("Error fetching Products:", error);
+        setIsLoading(false);
       }
     };
     fetchProducts();
   }, [apiBaseUrl, consumerKey, consumerSecret]);
+
+  if (isLoading) {
+    return( 
+      <Loading/>
+    );
+  }
   return (
     <>
       <h1>Shop Here</h1>
